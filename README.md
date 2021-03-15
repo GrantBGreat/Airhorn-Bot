9c8ae103b085f7e9186069daac9e2fd534baa25b
Grant <grant@rbgn.ca>
2021-03-15 12:55:50 -0400

    commented code 0_o

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ more commands coming soon lol
 
 4) Create a file named `.env` in the root folder of the bot, and enter `TOKEN=(the bot token you just copied)`.
 5) [Install node](https://nodejs.org/en/).
-6) Navigate to the folder with the bots code and run `npm i`, this will install the dependancys discord.js, dotenv, fs, and ms.
+6) Navigate to the folder with the bots code and run `npm i`, this will install the dependancys discord.js, discordjs/opus, dotenv, fs, and ms.
 7) You can now start the bot using `node .` in the bot's root folder.
 
 ### Updates
--- commands/airhorn.js
+++ commands/airhorn.js
@@ -1,12 +1,18 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args) => {
+  // check if the user is in vc
+    if (! message.member.voice.channel) return message.reply("are you so scared you wont enter a vc with me?");
+
+    // declair the vc
     var voiceChannel = message.member.voice.channel
 
+    // play the sound in the vc
     await voiceChannel.join().then(connection =>{
       const dispatcher = connection.play('./assets/testsound.mp3');
     }).catch(err => console.error(err));
 
+    // leave the vc
     await dispatcher.on("end", end => {
       voiceChannel.leave();
     });
