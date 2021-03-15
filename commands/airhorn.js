dfbede80678d5ae4e8ac79e0371de6314ce2472f
Grant <grant@rbgn.ca>
2021-03-15 09:30:57 -0400

    main bit of bot done

---
+++ commands/airhorn.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, args) => {
+    var voiceChannel = message.member.voice.channel
+}
+
+//The command's name
+module.exports.help = {
+  name: "airhorn"
+}
