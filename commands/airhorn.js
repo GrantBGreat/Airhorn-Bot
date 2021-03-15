b76e2b12fd407f22aed1380c98bf4a889e63aaf2
Grant <grant@rbgn.ca>
2021-03-15 12:48:30 -0400

    test

--- commands/airhorn.js
+++ commands/airhorn.js
@@ -2,6 +2,14 @@ const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args) => {
     var voiceChannel = message.member.voice.channel
+
+    await voiceChannel.join().then(connection =>{
+      const dispatcher = connection.play('./assets/testsound.mp3');
+    }).catch(err => console.error(err));
+
+    await dispatcher.on("end", end => {
+      voiceChannel.leave();
+    });
 }
 
 //The command's name
